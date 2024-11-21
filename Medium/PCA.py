ffb247b30cf033484a2c1810924b3eff58016434
ShadowCoder151 <shadowcoder023@gmail.com>
2024-11-21 08:51:43 +0530

    Updated PCA

--- Medium/PCA.py
+++ Medium/PCA.py
@@ -1,29 +1,19 @@
 import numpy as np 
 
 def standardize(data):
-	m, n = data.shape
-	
-	for i in range(n):
-		y = data[:, i]
-		M = np.mean(y)
-		S = np.std(y)
-		data[:, i] = (y - M) / S
-
-	return data
+	return (data - np.mean(data, axis=0)) / np.std(data, axis=0)
     
 	
 def pca(data: np.ndarray, k: int) -> list[list[int|float]]:
-	# Your code here
 	st_data = standardize(data)
 	cov_matrix = np.cov(st_data, rowvar=False)
-
 	values, vectors = np.linalg.eig(cov_matrix)
-	vectors = np.round(vectors, 4)
-
-	S = list(zip(values, vectors))
-	S.sort(reverse=True, key=lambda x: x[0])
-
-	return [x[1] for x in S[:k]]
+	
+	sorted_indices = np.argsort(values)[::-1]
+	vectors = vectors[:, sorted_indices]
+	
+	principal_components = vectors[:, :k]
+	return np.round(principal_components, 4).tolist()
 
 
 data = np.array([[1, 2], [3, 4], [5, 6]])
