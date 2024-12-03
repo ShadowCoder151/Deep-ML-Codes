c791b244704df217f6876bf724e83ad70b00e754
ShadowCoder151 <shadowcoder023@gmail.com>
2024-12-03 19:42:50 +0530

    Updated hypervector

--- Medium/hypervector.py
+++ Medium/hypervector.py
@@ -1,17 +1,34 @@
 import numpy as np
 
-def hv(dimension, seed):
-	np.random.seed(seed)
-	return np.random.choice([-1, 1], size=dimension)
+# def hv(dimension, seed):
+# 	np.random.seed(seed)
+# 	return np.random.choice([-1, 1], size=dimension)
 
+
+# def create_row_hv(row, dim, random_seeds):
+# 	ans = np.zeros(shape=(dim, ))
+	
+# 	for feature in random_seeds:
+# 		h1 = hv(dim, random_seeds[feature])
+# 		h2 = hv(dim, random_seeds[feature])
+
+# 		X = h1 * h2
+# 		ans += X
+	
+# 	ans = np.sign(ans)
+# 	return ans
 
 def create_row_hv(row, dim, random_seeds):
-	ans = np.ones(shape=(dim, ))
-	
+	ans = np.zeros(shape=(dim,))
+
 	for feature in random_seeds:
-		hyper = hv(dim, random_seeds[feature])
-		ans = ans * hyper
-	
+		np.random.seed(random_seeds[feature])
+		h1 = np.random.choice([-1, 1], size=dim)
+		h2 = np.random.choice([-1, 1], size=dim)  
+		X = h1 * h2
+		ans += X
+		
+	ans = np.where(ans >= 0, 1, -1)
 	return ans
 
 row = {"FeatureA": "value1", "FeatureB": "value2"}
