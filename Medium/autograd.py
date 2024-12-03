c25db94b2d4641535f0f20d8580b963f46637903
ShadowCoder151 <shadowcoder023@gmail.com>
2024-12-03 19:12:43 +0530

    Updated AutoGrad

--- Medium/autograd.py
+++ Medium/autograd.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class Value:
 	def __init__(self, data, _children=(), _op=''):
 		self.data = data
@@ -12,16 +10,55 @@ class Value:
 
 	def __add__(self, other):
 		 # Implement addition here
-		pass
+		other = other if isinstance(other, Value) else Value(other)
+		out = Value(self.data + other.data, (self, other), '+')
+		
+		def _backward():
+			self.grad += out.grad
+			other.grad += out.grad
+		out._backward = _backward
+		return out
 
 	def __mul__(self, other):
 		# Implement multiplication here
-		pass
+		other = other if isinstance(other, Value) else Value(other)
+		out = Value(self.data * other.data, (self, other), '+')
+
+		def _backward():
+			self.grad += out.grad * other.data
+			other.grad += out.grad * self.data
+		out._backward = _backward
+		return out
 
 	def relu(self):
 		# Implement ReLU here
-		pass
+		# other = other if isinstance(other, value) else Value(other)
+		out = Value(max(0, self.data), (self, ), 'ReLU')
+		
+		def _backward():
+			self.grad += out.grad * (out.data > 0)
+			# other.grad += out.grad
+		out._backward = _backward
+		return out
 
 	def backward(self):
 		# Implement backward pass here
-		pass
+		topo = []
+		visited = set()
+		
+		def topo_sort(v):
+			if v not in visited:
+				visited.add(v)
+				for ch in v._prev:
+					topo_sort(ch)
+				topo.append(v)
+		
+		topo_sort(self)
+		
+		self.grad = 1
+		for node in reversed(topo):
+			node._backward()
+
+a = Value(2);b = Value(3);c = Value(10);d = a + b * c  ;e = Value(7) * Value(2);f = e + d;g = f.relu()  
+g.backward()
+print(a,b,c,d,e,f,g)
